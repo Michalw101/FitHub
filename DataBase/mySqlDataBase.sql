29dc7689a34c8d0d87691a7e52efa949d18dccd0
michal359 <michal0548429273@gmail.com>
2024-06-30 01:10:03 +0300

    sql

--- DataBase/mySqlDataBase.sql
+++ DataBase/mySqlDataBase.sql
@@ -219,9 +219,9 @@ VALUES
 (2, '464e1a2683f24ff030f2deb7bb3452903ba9afb8cc67ad260e2efe6a42a8cf5f', '2a0280e31556715cbef22eca1b36ef15');
 
 
-SELECT * FROM classes NATURAL JOIN limits_in_class NATURAL JOIN trainers JOIN users where user_id = trainer_id
+SELECT * FROM classes NATURAL JOIN limits_in_class NATURAL JOIN trainers JOIN users where user_id = trainer_id;
 
-
+delete from limits_in_class where limits_id > 8;
 -- insert into trainers_waiting_list(user_id, first_name, last_name, email, phone, birth_date, gender, degree_link, experience, specialization, last_work_place, place_of_study, instegram_link, facebook_link, twitter_link)
 -- values (4, "a", "a", "michal0548429273@gmail.com", "050", '2004-06-03', "Female", "aaaa", "12", "blablabla", "d", "f", "f","f", "f");
 
